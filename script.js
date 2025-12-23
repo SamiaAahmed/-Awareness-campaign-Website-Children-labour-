133f3d4bb5a346ba2d3da65c45b482d7036309e5
Samia Ahmed <23-101162@students.eui.edu.eg>
2025-12-23 05:48:46 +0200

    section 2

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 document.getElementById("text1").innerHTML = "تسجيل الدخول";
 document.getElementById("text2").innerHTML = "اعرف المزيد";
 document.getElementById("text3").innerHTML = "الأزمة الصامتة";
+
+const shortText = 'تسلب عمالة الأطفال 152 مليون طفل طفولتهم وتعليمهم ومستقبلهم إنها تؤثر على نموهم الجسدي والعقلي وتديم دورات الفقر وتحرمهم من الحقوق الأساسية يعمل هؤلاء الأطفال في ظروف خطرة ويفوتون التعليم ويفقدو';
+
+const fullText = 'تسلب عمالة الأطفال 152 مليون طفل طفولتهم وتعليمهم ومستقبلهم إنها تؤثر على نموهم الجسدي والعقلي وتديم دورات الفقر وتحرمهم من الحقوق الأساسية يعمل هؤلاء الأطفال في ظروف خطرة ويفوتون التعليم ويفقدون فرصة بناء حياة أفضل. هذه الظاهرة تؤثر على المجتمعات بأكملها وتعيق التنمية المستدامة';
+
+document.getElementById("text4").innerHTML = shortText;
+document.getElementById("text5").innerHTML = "↓ اقرأ المزيد";
+
+let isExpanded = false;
+
+document.getElementById("text5").addEventListener("click", function() {
+    if (isExpanded) {
+
+        document.getElementById("text4").innerHTML = shortText;
+        document.getElementById("text5").innerHTML = "↓ اقرأ المزيد";
+        isExpanded = false;
+    } else {
+        document.getElementById("text4").innerHTML = fullText;
+        document.getElementById("text5").innerHTML = "↑ اقرأ أقل";
+        isExpanded = true;
+    }
+});
