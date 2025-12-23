95c624c1485998639d11bf55f2ebb1a6a338e6a8
Samia Ahmed <23-101162@students.eui.edu.eg>
2025-12-23 05:42:01 +0200

    header

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("text1").innerHTML = "تسجيل الدخول";
+document.getElementById("text2").innerHTML = "اعرف المزيد";
+document.getElementById("text3").innerHTML = "الأزمة الصامتة";
